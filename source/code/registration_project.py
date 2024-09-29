c13518ef122806188d8e3c30e25585e7a8859275
Annelies <110182091+Qievit@users.noreply.github.com>
2024-09-30 01:00:36 +0200

    Added imshow to demos, made outline of main
    Main file is a jupyter notebook to run to get our results. 1.6 project registration is too wordy and inefficient to use for our actual project

--- source/code/registration_project.py
+++ source/code/registration_project.py
@@ -9,7 +9,7 @@ import registration as reg
 from IPython.display import display, clear_output
 
 
-def rigid_reg_cc_demo(Img1,Img2):
+def rigid_reg_cc_demo(Img1,Img2,imshow=False):
 
     # read the fixed and moving images
     # change these in order to read different images
@@ -40,29 +40,29 @@ def rigid_reg_cc_demo(Img1,Img2):
 
     iterations = np.arange(1, num_iter+1)
     similarity = np.full((num_iter, 1), np.nan)
+    if imshow:
+        fig = plt.figure(figsize=(14,6))
 
-    fig = plt.figure(figsize=(14,6))
+        # fixed and moving image, and parameters
+        ax1 = fig.add_subplot(121)
 
-    # fixed and moving image, and parameters
-    ax1 = fig.add_subplot(121)
+        # fixed image
+        im1 = ax1.imshow(I)
+        # moving image
+        im2 = ax1.imshow(I, alpha=0.7)
+        # parameters
+        txt = ax1.text(0.3, 0.95,
+            np.array2string(x, precision=5, floatmode='fixed'),
+            bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+            transform=ax1.transAxes)
 
-    # fixed image
-    im1 = ax1.imshow(I)
-    # moving image
-    im2 = ax1.imshow(I, alpha=0.7)
-    # parameters
-    txt = ax1.text(0.3, 0.95,
-        np.array2string(x, precision=5, floatmode='fixed'),
-        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
-        transform=ax1.transAxes)
+        # 'learning' curve
+        ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
 
-    # 'learning' curve
-    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
-
-    learning_curve, = ax2.plot(iterations, similarity, lw=2)
-    ax2.set_xlabel('Iteration')
-    ax2.set_ylabel('Similarity')
-    ax2.grid()
+        learning_curve, = ax2.plot(iterations, similarity, lw=2)
+        ax2.set_xlabel('Iteration')
+        ax2.set_ylabel('Similarity')
+        ax2.grid()
 
     # perform 'num_iter' gradient ascent updates
     past_s = [0,0,0,0,0]
@@ -75,18 +75,21 @@ def rigid_reg_cc_demo(Img1,Img2):
         # for visualization of the result
         S, Im_t, _ = reg.rigid_corr(I, Im, x, return_transform=True)
 
-        clear_output(wait = True)
+        if imshow:
+            clear_output(wait = True)
 
-        # update moving image and parameters
-        im2.set_data(Im_t)
-        txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+            # update moving image and parameters
+            im2.set_data(Im_t)
+            txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+            # update 'learning' curve
+            similarity[k] = S
+            learning_curve.set_ydata(similarity)
+
+            display(fig)
 
         # update 'learning' curve
         similarity[k] = S
-        learning_curve.set_ydata(similarity)
-
-        display(fig)
-
+        
         # End if: similarity is high, or if stability has been reached
         past_s.append(S)
         del past_s[0]
@@ -99,7 +102,7 @@ def rigid_reg_cc_demo(Img1,Img2):
 
     print(f'Final similarity of ca. {S}')
 
-def affine_reg_cc_demo(img1,img2,num_iter=200, learning_rate=0.001):
+def affine_reg_cc_demo(img1,img2,imshow=False,num_iter=200, learning_rate=0.001):
 
     # read the fixed and moving images
     # change these in order to read different images
@@ -128,29 +131,29 @@ def affine_reg_cc_demo(img1,img2,num_iter=200, learning_rate=0.001):
 
     iterations = np.arange(1, num_iter+1)
     similarity = np.full((num_iter, 1), np.nan)
+    if imshow:
+        fig = plt.figure(figsize=(14,6))
 
-    fig = plt.figure(figsize=(14,6))
+        # fixed and moving image, and parameters
+        ax1 = fig.add_subplot(121)
 
-    # fixed and moving image, and parameters
-    ax1 = fig.add_subplot(121)
+        # fixed image
+        im1 = ax1.imshow(I)
+        # moving image
+        im2 = ax1.imshow(I, alpha=0.7)
+        # parameters
+        txt = ax1.text(0.3, 0.95,
+            np.array2string(x, precision=5, floatmode='fixed'),
+            bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+            transform=ax1.transAxes)
 
-    # fixed image
-    im1 = ax1.imshow(I)
-    # moving image
-    im2 = ax1.imshow(I, alpha=0.7)
-    # parameters
-    txt = ax1.text(0.3, 0.95,
-        np.array2string(x, precision=5, floatmode='fixed'),
-        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
-        transform=ax1.transAxes)
+        # 'learning' curve
+        ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
 
-    # 'learning' curve
-    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
-
-    learning_curve, = ax2.plot(iterations, similarity, lw=2)
-    ax2.set_xlabel('Iteration')
-    ax2.set_ylabel('Similarity')
-    ax2.grid()
+        learning_curve, = ax2.plot(iterations, similarity, lw=2)
+        ax2.set_xlabel('Iteration')
+        ax2.set_ylabel('Similarity')
+        ax2.grid()
 
     # perform 'num_iter' gradient ascent updates
     for k in np.arange(num_iter):
@@ -160,23 +163,23 @@ def affine_reg_cc_demo(img1,img2,num_iter=200, learning_rate=0.001):
         
         # for visualization of the result
         S, Im_t, _ = reg.affine_corr(I, Im, x, return_transform=True)
+        if imshow:
+            clear_output(wait = True)
 
-        clear_output(wait = True)
+            # update moving image and parameters
+            im2.set_data(Im_t)
+            txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
 
-        # update moving image and parameters
-        im2.set_data(Im_t)
-        txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+            # update 'learning' curve
+            similarity[k] = S
+            learning_curve.set_ydata(similarity)
 
-        # update 'learning' curve
-        similarity[k] = S
-        learning_curve.set_ydata(similarity)
-
-        display(fig)
-
+            display(fig)
+        similarity[k] = S  
     print(f'Final similarity of ca. {S}')
     return similarity
 
-def affine_reg_mi_demo(img1,img2,num_iter=200, learning_rate=0.001):
+def affine_reg_mi_demo(img1,img2,imshow=False,num_iter=200, learning_rate=0.001):
 
     # read the fixed and moving images
     # change these in order to read different images
@@ -205,29 +208,29 @@ def affine_reg_mi_demo(img1,img2,num_iter=200, learning_rate=0.001):
 
     iterations = np.arange(1, num_iter+1)
     similarity = np.full((num_iter, 1), np.nan)
+    if imshow:
+        fig = plt.figure(figsize=(14,6))
 
-    fig = plt.figure(figsize=(14,6))
+        # fixed and moving image, and parameters
+        ax1 = fig.add_subplot(121)
 
-    # fixed and moving image, and parameters
-    ax1 = fig.add_subplot(121)
+        # fixed image
+        im1 = ax1.imshow(I)
+        # moving image
+        im2 = ax1.imshow(I, alpha=0.7)
+        # parameters
+        txt = ax1.text(0.3, 0.95,
+            np.array2string(x, precision=5, floatmode='fixed'),
+            bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+            transform=ax1.transAxes)
 
-    # fixed image
-    im1 = ax1.imshow(I)
-    # moving image
-    im2 = ax1.imshow(I, alpha=0.7)
-    # parameters
-    txt = ax1.text(0.3, 0.95,
-        np.array2string(x, precision=5, floatmode='fixed'),
-        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
-        transform=ax1.transAxes)
+        # 'learning' curve
+        ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
 
-    # 'learning' curve
-    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
-
-    learning_curve, = ax2.plot(iterations, similarity, lw=2)
-    ax2.set_xlabel('Iteration')
-    ax2.set_ylabel('Similarity')
-    ax2.grid()
+        learning_curve, = ax2.plot(iterations, similarity, lw=2)
+        ax2.set_xlabel('Iteration')
+        ax2.set_ylabel('Similarity')
+        ax2.grid()
 
     past_s = [0,0,0,0,0]
     # perform 'num_iter' gradient ascent updates
@@ -240,18 +243,19 @@ def affine_reg_mi_demo(img1,img2,num_iter=200, learning_rate=0.001):
         # for visualization of the result
         S, Im_t, _ = reg.affine_mi(I, Im, x, return_transform=True)
 
-        clear_output(wait = True)
+        if imshow:
+            clear_output(wait = True)
 
-        # update moving image and parameters
-        im2.set_data(Im_t)
-        txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+            # update moving image and parameters
+            im2.set_data(Im_t)
+            txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
 
-        # update 'learning' curve
+            # update 'learning' curve
+            similarity[k] = S
+            learning_curve.set_ydata(similarity)
+
+            display(fig)
         similarity[k] = S
-        learning_curve.set_ydata(similarity)
-
-        display(fig)
-
         # End if: similarity is high, or if stability has been reached
         past_s.append(S)
         del past_s[0]
